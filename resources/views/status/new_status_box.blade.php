88156d501e9b8f5424ddd407f58d35824410e6b0
Nathan Johnson <nathancj224@gmail.com>
2016-03-07 00:30:31 -0500

    Cleaned up design of new status box

--- resources/views/status/new_status_box.blade.php
+++ resources/views/status/new_status_box.blade.php
@@ -1,9 +1,18 @@
-<div id="new-post" class="white-box">
+<div id="new-status" class="new-status white-box">
   <form action="{{ route('status.new') }}" method="post">
     {{ csrf_field() }}
-    <textarea class="form-control" placeholder="Write a status..." name="body"></textarea>
-    <br />
-    <button class="btn btn-primary pull-right">Post</button>
+    <textarea class="form-control" placeholder="What's up, {{ Auth::user()->username}}?" name="body"></textarea>
+    <div class="actions">
+      <div class="pull-left">
+        <button class="btn btn-default">
+            <span class="glyphicon glyphicon-picture"></span> Add Pictures
+        </button>
+        <button class="btn btn-default">
+            <span class="glyphicon glyphicon-folder-open"></span> Create Album
+        </button>
+      </div>
+      <button class="btn btn-primary pull-right">Post Status</button>
+    </div>
     <div class="clearfix"></div>
   </form>
 </div>
